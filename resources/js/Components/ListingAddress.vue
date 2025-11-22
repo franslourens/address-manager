a511f2cd1792abd1d6dd2a07ac0130f1e0d4b6e6
Frans Lourens <franslourens@Mac-mini.local>
2025-11-22 16:36:57 +0200

    list buttons

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span>
+      {{ address.line1 }} |
+    </span> 
+    <span>
+      {{ address.status }}
+    </span>
+    <span>
+      {{ address.last_error }}
+    </span> 
+  </div>
+  <span>
+    {{ address.line2 }}
+    {{ address.country_code }}
+    {{ address.city }}
+    {{ address.province }}
+    {{ address.postal }}
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  address: Object,
+})
+</script>
